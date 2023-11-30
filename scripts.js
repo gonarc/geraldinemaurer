fcd01c78369b192d5f884a9fd0e8237f755f7502
Gonzalo Arce <gonza_arc@hotmail.com>
2023-11-30 12:49:30 -0300

    Se agrego menu hamburguesa y funcionalidad js

---
+++ scripts.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollLinks = document.querySelectorAll('.scroll-link');
+
+    scrollLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('data-scroll-target');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
+
+var menuHamburguesa = document.getElementById("id-icon-nav");
+var abrirMenu = document.getElementById("id-container-nav-r");
+var cerrarMenu = document.querySelector(".container-nav-r");
+var menu1 = document.querySelector("#menu1");
+var menu2 = document.querySelector("#menu2");
+
+
+menuHamburguesa.addEventListener("click", () => {
+  abrirMenu.style.left = "0px";
+  menu1.style.opacity= "0";
+  menu2.style.opacity= "1";
+
+});
+cerrarMenu.addEventListener("click", () => {
+  abrirMenu.style.left = "-600px";
+  menu1.style.opacity= "1";
+  menu2.style.opacity= "0";
+});
+
